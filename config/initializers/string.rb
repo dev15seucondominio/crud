6da236553cf60199a27d26c00c1d9ca6f0557dc2
David <dbrahiam@gmail.com>
2016-06-17 10:52:14 -0300

    string: add fileize,remover acentos e somente_letras

--- config/initializers/string.rb
+++ config/initializers/string.rb
@@ -4,7 +4,25 @@ class String
     gsub(/[^0-9]/, '')
   end
 
+  def somente_letras
+    gsub(/[^a-zA-Z]/, '')
+  end
+
   def sem_numeros
     gsub(/[*0-9]/, '')
+  end
+
+  def remover_acentos
+    return self if self.blank?
+    texto = self
+    texto = texto.gsub(/(á|à|ã|â|ä)/, 'a').gsub(/(é|è|ê|ë)/, 'e').gsub(/(í|ì|î|ï)/, 'i').gsub(/(ó|ò|õ|ô|ö)/, 'o').gsub(/(ú|ù|û|ü)/, 'u')
+    texto = texto.gsub(/(Á|À|Ã|Â|Ä)/, 'A').gsub(/(É|È|Ê|Ë)/, 'E').gsub(/(Í|Ì|Î|Ï)/, 'I').gsub(/(Ó|Ò|Õ|Ô|Ö)/, 'O').gsub(/(Ú|Ù|Û|Ü)/, 'U')
+    texto = texto.gsub(/ñ/, 'n').gsub(/Ñ/, 'N')
+    texto = texto.gsub(/ç/, 'c').gsub(/Ç/, 'C')
+    texto
+  end
+
+  def fileize
+    remover_acentos.somente_letras.underscore.gsub(" ", "_")
   end
 end
